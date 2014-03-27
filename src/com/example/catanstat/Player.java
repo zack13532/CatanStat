f7c3248670e0454743c72e11f2c2af4a0d394231
Zachary Siegel <zack13532@gmail.com>
2014-03-27 00:36:47 -0400

    Added a StatMatrix field to the Player class.

--- src/com/example/catanstat/Player.java
+++ src/com/example/catanstat/Player.java
@@ -3,9 +3,11 @@ package com.example.catanstat;
 public class Player {
 
 		private String name;
+		private StatMatrix stats;
 		
 		public Player(String name){
 			
 			this.name = name;
+			this.stats = new StatMatrix();
 		}
 }
